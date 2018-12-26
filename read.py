0312aff23232b4d74a3d55eee57aca9d711ee695
YC Chen <chency1216@gmail.com>
2018-12-26 18:01:22 +0800

    average length of reviews

--- read.py
+++ read.py
@@ -2,11 +2,13 @@ data = []
 count = 0
 with open('reviews.txt', 'r') as f:
 	for line in f:
-		count += 1
+		count += 1 #讀取一筆留言就計數1
 		data.append(line)
-		if count % 10000 == 0:
+		if count % 100000 == 0:#顯示讀取每10萬筆
 			print(len(data))
-print(len(data))
-print(data[0])
-print('--------------------')
-print(data[1])
+print('讀取完畢總共有', len(data), '筆資料')
+
+sum_len = 0
+for d in data:
+	sum_len += len(d)
+print('平均留言長度:', sum_len/len(data))
